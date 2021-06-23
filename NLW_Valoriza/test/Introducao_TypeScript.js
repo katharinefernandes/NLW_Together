f6538a9c563bc5989a6fa20b254c43ad81700d65
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-06-23 03:02:11 -0300

    NLW-Together
    Atualização Aula 1

---
+++ NLW_Valoriza/test/Introducao_TypeScript.js
@@ -0,0 +1,13 @@
+//nomeDaVariável:TipoDaVariável
+//Ponto de interrogação, atributo opcional
+function enviarEmail(_a) {
+    var email = _a.email, nome = _a.nome, telefone = _a.telefone;
+    console.log("Ol\u00E1 " + nome + " seu email \u00E9 " + email + " e seu telefone \u00E9 " + telefone);
+    //
+    //
+    //
+}
+enviarEmail({
+    email: "katharinerodrigues2004if@gmail.com",
+    nome: "Katharine",
+});
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NLW_Together
- 
+# NLW-Together
+ Códigos desenvolvidos durante o evento NLW Together da Rocketseat na Trilha NodeJs
