6b1b9107213b84684d3f26435330512fcb3799d0
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-06-26 03:00:09 -0300

    Atualizações Projeto - Aula 2

---
+++ fora_github/notes.ts
@@ -0,0 +1,19 @@
+// Métodos que podem ser utilizados
+/**
+ * GET    => Buscar uma informação
+ * POST   => Inserir (Criar) uma informação
+ * PUT    => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação específica
+ */
+
+/**
+ * Tipos de parâmetros
+ * Routes Params => http://localhost:3000/produtos/12322312331
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom&
+ * 
+ * Body Params => {
+ * "name": "teclado",
+ * "description": "teclado bom"
+ * }
+ */
