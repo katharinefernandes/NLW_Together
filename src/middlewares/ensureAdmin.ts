d893b2406735a6b9222f9b4fd179cbf72650ccdc
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-06-27 07:01:05 -0300

    Atualizações Aula 5

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -3,10 +3,18 @@ import {
   Response,
   NextFunction
 } from "express"
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+export  async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+
+  const { user_id } = request;
+ 
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
+  
   //Verificar se o usuário é admin
-  const admin = true;
 
   if (admin) {
     return next();
--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,11 @@ import {
 } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router();
 
@@ -20,11 +25,22 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserComplimentsComplimentsController = new ListUserReceiveComplimentsController();
 
-router.post("/tags", ensureAdmin, createTagController.handle);
+const listTagsController = new ListTagsController();
+
+const listUsersController = new ListUsersController(); 
+
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
-router.post("/compliments", createComplimentController.handle);
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle);
+router.get("/users/compliments/receive", ensureAuthenticated, listUserComplimentsComplimentsController.handle);
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
+router.get("/users",ensureAuthenticated ,listUsersController.handle);
 
 export {
   router
